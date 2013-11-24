1871634b0620ff5f29c6654f0fe8d7c6f4c0d607
Ben Eggett <beneggett@gmail.com>
2013-11-24 01:28:22 -0700

    Adds support for Rails 4 in addition to rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SimpleContact::Engine.routes.draw do
   
   get "/", to: 'contact#new', as: :contact
-  post "/", to: 'contact#create', as: :contact
+  post "/", to: 'contact#create', as: :create_contact
   root to: 'contact#new'
 end
