27aa2f5fbe2ff120467e43d3f67396bc840137dd
Ben Eggett <beneggett@gmail.com>
2013-11-24 10:47:46 -0700

    Adds more comments & documentation, moves env variables over to travis, bumps to V 0.0.8

--- app/models/simple_contact/message.rb
+++ app/models/simple_contact/message.rb
@@ -1,14 +1,20 @@
-class SimpleContact::Message  
-
+class SimpleContact::Message   
+  # Simple Contact is a non-active record model, however we still use validations from active model as such.
   include ActiveModel::Validations
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
+  # Set's up the fields for our message
   attr_accessor :body, :email, :name, :subject
 
+  # Validates presence of all fields by default
   validates :body, :subject, :name, :email, presence: true
-  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
 
+  # Validates email format & displays a customizable error message from the translation file
+  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: I18n.translate(:invalid_email_message) }
+
+
+  # Setup code to make it work like an AR model would
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
