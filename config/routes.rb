c4c92abd79a31de4111e4f353c105f64034805c7
Ben Eggett <beneggett@gmail.com>
2014-02-26 09:51:15 -0700

    Changes create contact route, Minor version bump

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SimpleContact::Engine.routes.draw do
   # Routes are simple. All routes will point to the root_url, which will be where you mount the engine.
   get "/", to: 'contact#new', as: :contact
-  post "/", to: 'contact#create', as: :contact
+  post "/", to: 'contact#create', as: :create_contact
   root to: 'contact#new'
 end
